a9d362eee10dbea475622846b57acb223a38d320
apal6981 <ashton.palacios1@gmail.com>
2023-05-02 17:15:25 -0600

    working on parsing protocol

--- src/controller/song_dist.py
+++ src/controller/song_dist.py
@@ -1,9 +1,10 @@
 import serial
 import struct
-from mido import MidiFile
-import mido
+# from mido import MidiFile
+# import mido
+import time
 
-# ser = serial.Serial('COM6', 115200, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
 # if ser is None:
 #     exit(1)
@@ -12,31 +13,55 @@ def generate_anncoucement_bytes(part: int, part_len: list) -> bytes:
     # return struct.pack("<bbbbbb",((part & 0b11)<< 6) | (part_0_len & 0b1111110000)>>4,((part_0_len & 0b1111)<< 4) | (part_1_len& 0b1111000000)>>4,
     #                               (part_1_len & 0b111111)<<2 | (part_2_len & 0b1100000000)>>8,(part_2_len & 0b11111111),
     #                               (part_3_len & 0b1111111100)>>2,(part_3_len&0b11)<<6)
-    return struct.pack("<B"+"H"*len(part_len),part,*part_len)
+    return struct.pack("!B"+"H"*len(part_len),part,*part_len)
 
-def generate_song_bytes(part:int, sequence:int,note:int,red:int,green:int,blue:int,duration:int) -> bytes:
-    return struct.pack("<BHBBBBH",part,sequence,note,red,green,blue,duration)
+def generate_song_bytes(part:int, sequence:int,duration:int,red:int,green:int,blue:int,note:int) -> bytes:
+    return struct.pack("!HHBBBBB",sequence,duration,part,red,green,blue,note)
 
 def checksum_calc(payload:bytes) -> bytes:
     checksum = 0
     for i in range(0,len(payload),2):
-        checksum += struct.unpack("<H",payload[i:i+2])[0]
-    return struct.pack("<H",checksum^0xFFFF)
+        checksum += struct.unpack("!H",payload[i:i+2])[0]
+    print(checksum,~checksum&0xFFFF)
+    return struct.pack("!H",~checksum&0xFFFF)
 
 def add_header(p_type: int, num_payload: int, repeat: int) -> bytes:
-    return checksum_calc + struct.pack("<B",(p_type & 0b111) << 5 | (num_payload & 0b1111) << 1 | (repeat & 0b1))
+    return struct.pack("!B",(p_type & 0b111) << 5 | (num_payload & 0b1111) << 1 | (repeat & 0b1))
 
 def generate_song_packet(part: int, packets: list, repeat: int) -> bytes:
     packet = b""
     for pack in packets:
         packet += generate_song_bytes(part,*pack)
-    return add_header(1,len(packets),repeat) + packet
+    packet = add_header(1,len(packets),repeat) + packet
+    if len(packet) % 2:
+        packet += struct.pack("!b",0)
+    packet = checksum_calc(packet) + packet
+    # print(len(packet),packet, checksum_calc(packet) + packet)
+    # return checksum_calc(packet) + packet
+    return struct.pack("!B",len(packet)) + packet
+
+def generate_anouncement_packet(part: int,part_len: list, repeat: int) -> bytes:
+    packet = b""
+    packet = add_header(0,1,repeat) + generate_anncoucement_bytes(part, part_len)
+    packet = checksum_calc(packet) + packet
+    print(len(packet),packet)
+    return struct.pack("!B",len(packet)) + packet
     
 
 
 
 if __name__ == "__main__":
-    print(generate_anncoucement_bytes(1,[1,1,1,1]))
+    # ser.write(generate_anouncement_packet(4,[1,1,1,1],0))
+    ser.write(generate_song_packet(0,[[0,300,255,255,255,60],[1,250,128,128,128,45]],1))
+    # print(generate_song_packet(0,[[255,255,255,60,0,300]],0))
+    # print(generate_anouncement_packet(4,[1,1,1,1],0))
+    # ser.write(generate_anncoucement_bytes(1,[1]))
+    # print(generate_anncoucement_bytes(1,[1]))
+    time.sleep(1)
+    while True:
+        print(ser.read_all())
+        time.sleep(1)
+    # print(generate_anncoucement_bytes(1,[1,1,1,1]))
     # pirates = MidiFile("moonlight.mid",clip=True)
     # print(pirates.length)
     # print(pirates.tracks)
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,48 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+typedef struct
+{
+  unsigned int p_type : 3;
+  unsigned int num_packets : 4;
+  unsigned int repeat : 1;
+} header;
+
+typedef struct
+{
+  uint16_t sequence;
+  uint16_t duration;
+  uint8_t part;
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t note;
+} song_note;
+
 void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen);
 void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status);
 void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength);
-void broadcast(char *message,size_t len);
+void broadcast(char *message, size_t len);
 void setup_broadcaster();
+uint16_t checksum_calc(uint16_t *data, int dataLen);
+uint8_t check_checksum(uint16_t *data, int dataLen);
+void get_header(header &head, uint8_t data);
+void get_song_packets(song_note *packets, uint8_t *data, uint8_t num_packets);
 
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t *message_buffer;
 size_t message_len;
 
-void setup() {
-  message_buffer = (uint8_t *)malloc(250*sizeof(uint8_t));
+void setup()
+{
+  message_buffer = (uint8_t *)malloc(250 * sizeof(uint8_t));
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(5000);
 
   Serial.println("ESPNow Example");
   WiFi.mode(WIFI_STA);
-  
+
   // Output my MAC address - useful for later
   Serial.print("My MAC Address is: ");
   Serial.println(WiFi.macAddress());
@@ -42,15 +65,87 @@ void setup() {
   setup_broadcaster();
 }
 
-
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   // Serial.println(millis());
   // broadcast(buffer,10);
   // delay(1000);
+  if (Serial.available() > 0)
+  {
+    char first[1];
+    // Serial.printf("%d ", Serial.available());
+    Serial.readBytes(first, 1);
+    uint8_t num_bytes = (uint8_t)first[0];
+    char _bytes[num_bytes];
+    int read = Serial.readBytes(_bytes, num_bytes);
+    // Serial.write(_bytes);
+    uint16_t *_16bytes = (uint16_t *)_bytes;
+    // Serial.printf("%d %d  %d \t%d ", ntohs(_16bytes[0]), ntohs(_16bytes[1]), ntohs(_16bytes[2]), read);
+    if (!check_checksum(_16bytes, num_bytes / 2 - 1))
+    {
+      Serial.println("Checksum failed");
+      return;
+    }
+    // Serial.printf("%d %d %d + %d = %d, %d", num_bytes, num_bytes / 2 - 1, ntohs(_16bytes[0]), checksum_calc(_16bytes + 1, num_bytes / 2 - 1), ntohs(_16bytes[0]) + checksum_calc(_16bytes + 1, num_bytes / 2 - 1), check_checksum(_16bytes, num_bytes / 2 - 1));
+    header p_head;
+    get_header(p_head, (uint8_t)_bytes[2]);
+    Serial.printf("%d %d %d    ", p_head.p_type, p_head.num_packets, p_head.repeat);
+    song_note *notes = (song_note *)malloc(2 * sizeof(song_note));
+    get_song_packets(notes, (uint8_t *)(_bytes + 3), p_head.num_packets);
+    Serial.printf("%d %d %d %d %d %d %d     %d %d %d %d %d %d %d", notes[0].sequence, notes[0].duration, notes[0].part, notes[0].red, notes[0].green, notes[0].blue, notes[0].note, notes[1].sequence, notes[1].duration, notes[1].part, notes[1].red, notes[1].green, notes[1].blue, notes[1].note);
+    // char num_bytes[1];
+    // Serial.readBytes(num_bytes, 1); // get how many bytes to read
+    // char send_bytes[(uint8_t)num_bytes[0] + 1];
+    // Serial.readBytes(send_bytes, (uint8_t)num_bytes[0] + 1);
+    // Serial.write(send_bytes);
+    // Serial.flush();
+    // Serial.printf("%d %x\n", Serial.available(), num_bytes);
+    // Serial.end();
+    // Serial.begin(115200);
+  }
 }
 
-void setup_broadcaster() {
+void get_song_packets(song_note *packets, uint8_t *data, uint8_t num_packets)
+{
+  uint8_t index = 0;
+  for (int i = 0; i < num_packets; i++)
+  {
+    packets[i].sequence = ntohs(*((uint16_t *)((data + index))));
+    packets[i].duration = ntohs(*((uint16_t *)((data + index + 2))));
+    packets[i].part = data[index + 4];
+    packets[i].red = data[index + 5];
+    packets[i].green = data[index + 6];
+    packets[i].blue = data[index + 7];
+    packets[i].note = data[index + 8];
+    index += 9;
+  }
+}
+
+void get_header(header &head, uint8_t data)
+{
+  head.p_type = (data & 0b11100000) >> 5;
+  head.num_packets = (data & 0b00011110) >> 1;
+  head.repeat = data & 0b1;
+}
+
+uint16_t checksum_calc(uint16_t *data, int dataLen)
+{
+  uint16_t checksum = 0;
+  for (int i = 0; i < dataLen; i++)
+  {
+    checksum += ntohs(data[i]);
+  }
+  return checksum;
+}
+
+uint8_t check_checksum(uint16_t *data, int dataLen)
+{
+  return (ntohs(data[0]) + checksum_calc(data + 1, dataLen)) == 0xFFFF ? 1 : 0;
+}
+
+void setup_broadcaster()
+{
   esp_now_peer_info_t peerInfo = {};
   memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
   if (!esp_now_is_peer_exist(broadcastAddress))
@@ -59,15 +154,15 @@ void setup_broadcaster() {
   }
 }
 
-void broadcast(char *message,size_t len) {
+void broadcast(char *message, size_t len)
+{
   // this will broadcast a message to everyone in range
-  
-  
-  esp_err_t result = esp_now_send(broadcastAddress,(const uint8_t *) message, len);
 
+  esp_err_t result = esp_now_send(broadcastAddress, (const uint8_t *)message, len);
 }
 
-void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen) {
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
+{
   char buffer[ESP_NOW_MAX_DATA_LEN + 1];
   int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
   strncpy(buffer, (const char *)data, msgLen);
@@ -80,7 +175,8 @@ void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen) {
   Serial.printf("Received message from: %s - %s\n", macStr, buffer);
 }
 
-void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status) {
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
+{
   char macStr[18];
   formatMacAddress(macAddr, macStr, 18);
   Serial.print("Last Packet Sent to: ");
@@ -89,6 +185,7 @@ void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength) {
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
+{
   snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 }
